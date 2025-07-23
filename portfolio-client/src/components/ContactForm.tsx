84487f3176c0d1cf728af96cb2d34aa6f4e4b841
KiyokoNee <k.c.gearing@gmail.com>
2025-07-23 16:56:13 -0500

    updated contact form regex

--- portfolio-client/src/components/ContactForm.tsx
+++ portfolio-client/src/components/ContactForm.tsx
@@ -48,7 +48,7 @@ export const ContactForm = () => {
                 register("email", {
                     required: "Email is required",
                     pattern: {
-                        value: /^[a-zA-Z0-9]+@(?:[a-zA-Z0-9]+\.)+com+$/,
+                        value: /^(?!\.)("[^"\r\\]*(?:\\.[^"\r\\]*)*"|[-a-zA-Z0-9!#$%&'*+/=?^_`{|}~]+(?:\.[-a-zA-Z0-9!#$%&'*+/=?^_`{|}~]+)*)@(?!-)(?:[a-zA-Z0-9-]+\.)+[a-zA-Z]{2,}$/,
                         message: "Invalid email"
                     }
                 })}
