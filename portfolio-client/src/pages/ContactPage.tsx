88c0465b6b20f250a1ff2190838cebfb1cfe776f
KiyokoNee <k.c.gearing@gmail.com>
2025-08-14 15:54:54 -0500

    minor change to the contact page phrasing

--- portfolio-client/src/pages/ContactPage.tsx
+++ portfolio-client/src/pages/ContactPage.tsx
@@ -5,7 +5,7 @@ export const ContactPage = () => {
         <div className="flex flex-col items-center justify-center min-h-screen py-12 bg-cover bg-center"
         >
             <div className="shadow-lg rounded-lg p-8 w-full max-w-lg">
-                <h1 className="text-4xl font-bold text-center mb-6">Contact Us</h1>
+                <h1 className="text-4xl font-bold text-center mb-6">Contact Me!</h1>
                 <p className="text-center mb-4">
                     I would love to hear from you! Please fill out the form below or contact me directly:
                 </p>
